00278182d987b63abb0acbdf5f1988e9c9c35437
Hinata Chikao <StubbornStruggle@gmail.com>
2026-08-06 01:34:46 +0330

    change commit shell script

--- commit.sh
+++ commit.sh
@@ -1,6 +1,24 @@
-#!/bin/env bash
+#!/usr/bin/env zsh
+
+set -e
+
+if [[ $# -eq 0 ]]; then
+    echo "error: commit message is required"
+    echo "usage: $0 commit message"
+    exit 1
+fi
+
+message="$*"
 
 rm -rf src/.main.rs.bak*
+
 git add --all
-git commit -m 'New Rust sample code for review'
+
+if git diff --cached --quiet; then
+    echo "nothing to commit"
+    git status
+    exit 0
+fi
+
+git commit -m "$message"
 git status
