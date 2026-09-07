14666e071c2e01414381da083b40609e1d1396a3
HinataDeveloper <StubbornStruggle@gmail.com>
2026-09-07 05:08:06 +0330

    update mp.rs file

--- mp.rs
+++ mp.rs
@@ -1,6 +1,32 @@
+// Date: Thu Sep 07 2026
+
+// Project: Learning Chapter 13
+// Goal: Using File IO: Using BufReader
+// Dependency: Without dependency
+
+// rustc 1.100.0-nightly (f248f4038 2026-09-05)
+// binary: rustc
+// commit-hash: f248f4038796913873f11ca65b1b901e311c8dae
+// commit-date: 2026-09-05
+// host: x86_64-unknown-linux-gnu
+// release: 1.100.0-nightly
+// LLVM version: 23.1.1
+
+// cargo 1.100.0-nightly (3c0b53475 2026-09-04)
+// release: 1.100.0-nightly
+// commit-hash: 3c0b534756e166d12eb9fd2e1abfe5b42ac6101e
+// commit-date: 2026-09-04
+// host: x86_64-unknown-linux-gnu
+// libgit2: 1.9.6 (sys:0.21.0 vendored)
+// libcurl: 8.21.0-DEV (sys:0.4.90+curl-8.21.0 vendored ssl:OpenSSL/3.6.3)
+// ssl: OpenSSL 3.6.3 9 Jun 2026
+// os: Fedora 44.0.0 [64-bit]
+
+// Kernel Version: 7.1.13-200.fc44.x86_64
+// Firmware Version: 71CN51WW(V1.21)
 
 fn main() {
     println!("\n");
 
-    println!("\nThe End ...");
+    println!("\nThe End ...\n");
 }
